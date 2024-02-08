ebe2c0761d9a49f7107efcabe112a796096bcc4d
rahmanny <a.rahmanny@gmail.com>
2024-02-08 09:57:56 +0400

    Add XML Doc

--- src/Response/DefaultResponse.cs
+++ src/Response/DefaultResponse.cs
@@ -1,27 +1,48 @@
 ﻿using System.Collections.Generic;
 using System.Collections.Immutable;
+using System.Diagnostics;
 using System.Linq;
 using DefaultLibrary.Shared;
 
 namespace DefaultLibrary.Response
 {
+    /// <summary>
+    /// Abstract class for response objects
+    /// </summary>
     public abstract class DefaultResponse
     {
         private readonly List<Error> _errors = new List<Error>();
         
+        /// <summary>
+        /// Indicates if the response has errors
+        /// </summary>
         public bool HasErrors => _errors.Any();
+        /// <summary>
+        /// Status of the response
+        /// </summary>
         public Status Status { get; private set; }
+        /// <summary>
+        /// List of errors
+        /// </summary>
         public IImmutableList<Error> Errors => _errors.ToImmutableList();
 
-        protected void AddError(string? message, string[]? properties, string? method)
+        /// <summary>
+        /// Adds an error to the response
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="properties"></param>
+        /// <param name="method"></param>
+        /// <param name="stackTrace"></param>
+        public void AddError(string message, string[]? properties = null, string? method = null, StackTrace? stackTrace = null)
         {
             _errors.Add(new Error
             {
                 Message = message,
                 Properties = properties,
-                Method = method
+                Method = method,
+                StackTrace = stackTrace
             });
+            Status = Status.Error;
         }
-        
     }
 }
--- src/Shared/Error.cs
+++ src/Shared/Error.cs
@@ -1,9 +1,28 @@
-﻿namespace DefaultLibrary.Shared
+﻿using System.Diagnostics;
+
+namespace DefaultLibrary.Shared
 {
+    /// <summary>
+    /// Error object
+    /// </summary>
     public class Error
     {
+        
+        /// <summary>
+        /// Error message
+        /// </summary>
         public string? Message { get; set; }
+        /// <summary>
+        /// Properties that caused the error
+        /// </summary>
         public string[]? Properties { get; set; }
+        /// <summary>
+        /// Method that caused the error
+        /// </summary>
         public string? Method { get; set; }
+        /// <summary>
+        /// Stack trace of the error
+        /// </summary>
+        public StackTrace? StackTrace { get; set; }
     }
 }
