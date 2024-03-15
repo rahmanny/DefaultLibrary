b563b068102c8e8e595cdca7b7f623a23a5eab58
rahmanny <a.rahmanny@gmail.com>
2024-03-15 16:13:31 +0400

    Add Default EFCore Entiry

---
+++ src/Table/DefaultTable.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DefaultLibrary.Table;
+
+/// <summary>
+/// Abstract class for Entity objects
+/// </summary>
+public abstract class DefaultTable
+{
+    /// <summary>
+    /// Id of the entity
+    /// </summary>
+    public Guid Id { get; set; }
+    /// <summary>
+    /// Created at
+    /// </summary>
+    public DateTime CreatedAt { get; set; }
+    /// <summary>
+    /// Updated at, nullable
+    /// </summary>
+    public DateTime? UpdatedAt { get; set; }
+
+}
